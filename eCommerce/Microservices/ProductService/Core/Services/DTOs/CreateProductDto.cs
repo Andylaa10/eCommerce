8ab80f0b81b0a11bbeb5b6579289dc2e9195645d
KristianHollaender <90904724+KristianHollaender@users.noreply.github.com>
2024-05-07 13:31:24 +0200

    ProductService done

---
+++ eCommerce/Microservices/ProductService/Core/Services/DTOs/CreateProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductService.Core.Services.DTOs;
+
+public class CreateProductDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public float Price { get; set; }
+    public bool InStock { get; set; }
+    public int NumberInStock { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
