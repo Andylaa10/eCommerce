325df0c5ed708fce7f56d4238a9abbdb9913457c
Andylaa10 <andylaa100@gmail.com>
2024-05-07 14:18:50 +0200

    auth service is done

--- eCommerce/Microservices/AuthService/Controllers/AuthController.cs
+++ eCommerce/Microservices/AuthService/Controllers/AuthController.cs
@@ -1,3 +1,5 @@
+using AuthService.Core.Services.DTOs;
+using AuthService.Core.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthService.Controllers;
@@ -6,8 +8,66 @@ namespace AuthService.Controllers;
 [Route("api/[controller]")]
 public class AuthController : ControllerBase
 {
-    public AuthController()
+    private readonly IAuthService _authService;
+    public AuthController(IAuthService authService)
     {
+        _authService = authService;
+    }
+
+    [HttpPost]
+    [Route("Register")]
+    public async Task<IActionResult> Register([FromBody] CreateAuthDto dto)
+    {
+        try
+        {
+            await _authService.Register(dto);
+            return StatusCode(201, "Successfully registered");
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    [HttpPost]
+    [Route("Login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto dto)
+    {
+        try
+        {
+            return Ok(await _authService.Login(dto));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    [HttpGet]
+    [Route("ValidateToken")]
+    public async Task<bool> ValidateToken()
+    {
+        try
+        {
+            // checking for a valid token in the Authorization header
+            var re = Request;
+
+            if (!re.Headers.ContainsKey("Authorization"))
+                return await Task.Run(() => false);
+
+            if (!re.Headers["Authorization"].ToString().StartsWith("Bearer "))
+                return await Task.Run(() => false);
+
+            
+            // decode the token & check if it's valid
+            var token = re.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            var result = await _authService.ValidateToken(token);
+            return result.Succeeded;
+        }
+        catch (Exception e)
+        {
+            return await Task.Run(() => false);
+        }
     }
     
 }
---
+++ eCommerce/Microservices/AuthService/Core/Entities/Auth.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Core.Entities;
+
+public class Auth
+{
+    public int Id { get; set; }
+    [EmailAddress] public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string Salt { get; set; }
+}
