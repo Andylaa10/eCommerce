464dab667df33efde962d0ce10fa59e438653b79
KristianHollaender <90904724+KristianHollaender@users.noreply.github.com>
2024-06-04 12:09:16 +0200

    Added monitoring to user service

--- eCommerce/Microservices/UserService/Controllers/UserController.cs
+++ eCommerce/Microservices/UserService/Controllers/UserController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using MonitoringService;
-using OpenTelemetry.Trace;
 using UserService.Core.Services.DTOs;
 using UserService.Core.Services.Interfaces;
 
@@ -11,63 +9,49 @@ namespace UserService.Controllers;
 public class UserController : ControllerBase
 {
     private readonly IUserService _userService;
-    private readonly Tracer _tracer;
-    
-    public UserController(IUserService userService, Tracer tracer)
+
+    public UserController(IUserService userService)
     {
         _userService = userService;
-        _tracer = tracer;
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateUser([FromBody] CreateUserDto dto)
     {
-        using var activity = _tracer.StartActiveSpan("CreateUser");
-        
         try
         {
-            LoggingService.Log.Information("Called CreateUser Method");
             return StatusCode(201, await _userService.AddUser(dto));
         }
         catch (Exception e)
         {
-            LoggingService.Log.Error(e.Message);
             return BadRequest(e.ToString());
         }
     }
-    
+
     [HttpGet]
     [Route("{id}")]
     public async Task<IActionResult> GetUserById([FromRoute] int id)
     {
-        using var activity = _tracer.StartActiveSpan("GetUserById");
-
         try
         {
-            LoggingService.Log.Information("Called GetUserById Method");
             return Ok(await _userService.GetUserById(id));
         }
         catch (Exception e)
         {
-            LoggingService.Log.Error(e.Message);
             return BadRequest(e.ToString());
         }
     }
-    
+
     [HttpGet]
     [Route("Users")]
     public async Task<IActionResult> GetAllUsers()
     {
-        using var activity = _tracer.StartActiveSpan("GetAllUsers");
-
         try
         {
-            LoggingService.Log.Information("Called GetAllUsers Method");
             return Ok(await _userService.GetAllUsers());
         }
         catch (Exception e)
         {
-            LoggingService.Log.Error(e.Message);
             return BadRequest(e.ToString());
         }
     }
@@ -76,16 +60,12 @@ public class UserController : ControllerBase
     [Route("{id}")]
     public async Task<IActionResult> UpdateUser([FromRoute] int id, [FromBody] UpdateUserDto dto)
     {
-        using var activity = _tracer.StartActiveSpan("UpdateUser");
-
         try
         {
-            LoggingService.Log.Information("Called UpdateUser Method");
             return Ok(await _userService.UpdateUser(id, dto));
         }
         catch (Exception e)
         {
-            LoggingService.Log.Error(e.Message);
             return BadRequest(e.ToString());
         }
     }
@@ -94,37 +74,28 @@ public class UserController : ControllerBase
     [Route("{id}")]
     public async Task<IActionResult> DeleteUser([FromRoute] int id)
     {
-        using var activity = _tracer.StartActiveSpan("DeleteUser");
-
         try
         {
-            LoggingService.Log.Information("Called DeleteUser Method");
             return Ok(await _userService.DeleteUser(id));
         }
         catch (Exception e)
         {
-            LoggingService.Log.Error(e.Message);
             return BadRequest(e.ToString());
         }
     }
-    
+
     [HttpPost]
     [Route("Rebuild")]
     public async Task<IActionResult> RebuildDatabase()
     {
-        using var activity = _tracer.StartActiveSpan("RebuildDatabase");
-
         try
         {
-            LoggingService.Log.Information("Called RebuildDatabase Method");
             await _userService.RebuildDatabase();
             return Ok();
         }
         catch (Exception e)
         {
-            LoggingService.Log.Error(e.Message);
             return BadRequest(e.ToString());
         }
     }
-
 }
--- eCommerce/Microservices/UserService/Core/Services/UserService.cs
+++ eCommerce/Microservices/UserService/Core/Services/UserService.cs
@@ -3,6 +3,8 @@ using AutoMapper;
 using Cache;
 using Messaging;
 using Messaging.SharedMessages;
+using MonitoringService;
+using OpenTelemetry.Trace;
 using UserService.Core.Entities;
 using UserService.Core.Repositories.Interfaces;
 using UserService.Core.Services.DTOs;
@@ -16,100 +18,173 @@ public class UserService : IUserService
     private readonly MessageClient _messageClient;
     private readonly IRedisClient _redisClient;
     private readonly IUserRepository _userRepository;
+    private readonly Tracer _tracer;
 
     public UserService(IUserRepository userRepository, IMapper mapper, IRedisClient redisClient,
-        MessageClient messageClient)
+        MessageClient messageClient, Tracer tracer)
     {
         _userRepository = userRepository;
         _mapper = mapper;
         _redisClient = redisClient;
         _messageClient = messageClient;
+        _tracer = tracer;
         _redisClient.Connect();
     }
 
 
     public async Task<GetUserDto> GetUserById(int id)
     {
+        using var activity = _tracer.StartActiveSpan("GetUserById");
+
         if (id < 1)
             throw new ArgumentException("Id could be less than 0");
 
-        var userJson = await _redisClient.GetValue($"User:{id}");
+        try
+        {
+            LoggingService.Log.Information("Called GetUserById Method");
 
-        if (!string.IsNullOrEmpty(userJson))
-            return await Task.FromResult(_redisClient.DeserializeObject<GetUserDto>(userJson)!);
+            var userJson = await _redisClient.GetValue($"User:{id}");
 
-        var user = _mapper.Map<GetUserDto>(await _userRepository.GetUserById(id));
+            if (!string.IsNullOrEmpty(userJson))
+                return await Task.FromResult(_redisClient.DeserializeObject<GetUserDto>(userJson)!);
 
-        return user;
+            var user = _mapper.Map<GetUserDto>(await _userRepository.GetUserById(id));
+
+            return user;
+        }
+        catch (Exception e)
+        {
+            LoggingService.Log.Error(e.Message);
+            throw new ArgumentException(e.Message);
+        }
     }
 
     public async Task<IEnumerable<GetUserDto>> GetAllUsers()
     {
-        var users = await _userRepository.GetAllUsers();
-        return _mapper.Map<IEnumerable<GetUserDto>>(users);
+        using var activity = _tracer.StartActiveSpan("GetAllUsers");
+
+        try
+        {
+            LoggingService.Log.Information("Called GetAllUsers Method");
+
+            var users = await _userRepository.GetAllUsers();
+            return _mapper.Map<IEnumerable<GetUserDto>>(users);
+        }
+        catch (Exception e)
+        {
+            LoggingService.Log.Error(e.Message);
+            throw new ArgumentException(e.Message);
+        }
     }
 
     public async Task<GetUserDto> AddUser(CreateUserDto dto)
     {
+        using var activity = _tracer.StartActiveSpan("AddUser");
+
         var exists = await _userRepository.DoesUserExist(dto.Email);
 
         if (exists)
             throw new DuplicateNameException($"{dto.Email} is already in use");
 
-        var user = _mapper.Map<GetUserDto>(await _userRepository.AddUser(_mapper.Map<User>(dto)));
+        try
+        {
+            LoggingService.Log.Information("Called AddUser Method");
 
-        var userJson = _redisClient.SerializeObject(user);
-        await _redisClient.StoreValue($"User:{user.Id}", userJson);
+            var user = _mapper.Map<GetUserDto>(await _userRepository.AddUser(_mapper.Map<User>(dto)));
 
-        const string exchangeName = "CreateCartExchange";
-        const string routingKey = "CreateCart";
+            var userJson = _redisClient.SerializeObject(user);
+            await _redisClient.StoreValue($"User:{user.Id}", userJson);
 
+            const string exchangeName = "CreateCartExchange";
+            const string routingKey = "CreateCart";
 
-        _messageClient.Send(new CreateCartMessage("Create Cart", user.Id, 0.0f), exchangeName, routingKey);
+            _messageClient.Send(new CreateCartMessage("Create Cart", user.Id, 0.0f), exchangeName, routingKey);
 
-        return user;
+            return user;
+        }
+        catch (Exception e)
+        {
+            LoggingService.Log.Error(e.Message);
+            throw new ArgumentException(e.Message);
+        }
     }
 
     public async Task<GetUserDto> UpdateUser(int id, UpdateUserDto dto)
     {
+        using var activity = _tracer.StartActiveSpan("UpdateUser");
+
         if (id < 1)
             throw new ArgumentException("Id could be less than 0");
 
         if (id != dto.Id)
             throw new ArgumentException("Id int the route does not match the id of the user");
 
-        var user = _mapper.Map<GetUserDto>(await _userRepository.UpdateUser(id, _mapper.Map<User>(dto)));
+        try
+        {
+            LoggingService.Log.Information("Called UpdateUser Method");
 
-        var userJson = _redisClient.SerializeObject(user);
-        await _redisClient.StoreValue($"User:{id}", userJson);
+            var user = _mapper.Map<GetUserDto>(await _userRepository.UpdateUser(id, _mapper.Map<User>(dto)));
 
-        return user;
+            var userJson = _redisClient.SerializeObject(user);
+            await _redisClient.StoreValue($"User:{id}", userJson);
+
+            return user;
+        }
+        catch (Exception e)
+        {
+            LoggingService.Log.Error(e.Message);
+            throw new ArgumentException(e.Message);
+        }
     }
 
     public async Task<GetUserDto> DeleteUser(int id)
     {
+        using var activity = _tracer.StartActiveSpan("DeleteUser");
+
         if (id < 1)
             throw new ArgumentException("Id could be less than 0");
 
-        var user = _mapper.Map<GetUserDto>(await _userRepository.DeleteUser(id));
+        try
+        {
+            LoggingService.Log.Information("Called DeleteUser Method");
 
-        await _redisClient.RemoveValue($"User:{id}");
+            var user = _mapper.Map<GetUserDto>(await _userRepository.DeleteUser(id));
 
-        const string exchangeNameCart = "DeleteCartExchange";
-        const string routingKeyCart = "DeleteCart";
+            await _redisClient.RemoveValue($"User:{id}");
 
-        _messageClient.Send(new DeleteCartMessage("Delete Cart", user.Id), exchangeNameCart, routingKeyCart);
+            const string exchangeNameCart = "DeleteCartExchange";
+            const string routingKeyCart = "DeleteCart";
 
-        const string exchangeNameAuth = "DeleteAuthExchange";
-        const string routingKeyAuth = "DeleteAuth";
-        
-        _messageClient.Send(new DeleteAuthMessage("Delete Auth", id), exchangeNameAuth, routingKeyAuth);
+            _messageClient.Send(new DeleteCartMessage("Delete Cart", user.Id), exchangeNameCart, routingKeyCart);
 
-        return user;
+            const string exchangeNameAuth = "DeleteAuthExchange";
+            const string routingKeyAuth = "DeleteAuth";
+
+            _messageClient.Send(new DeleteAuthMessage("Delete Auth", id), exchangeNameAuth, routingKeyAuth);
+
+            return user;
+        }
+        catch (Exception e)
+        {
+            LoggingService.Log.Error(e.Message);
+            throw new ArgumentException(e.Message);
+        }
     }
 
     public async Task RebuildDatabase()
     {
-        await _userRepository.RebuildDatabase();
+        using var activity = _tracer.StartActiveSpan("RebuildUserDatabase");
+
+        try
+        {
+            LoggingService.Log.Information("Called RebuildUserDatabase Method");
+
+            await _userRepository.RebuildDatabase();
+        }
+        catch (Exception e)
+        {
+            LoggingService.Log.Error(e.Message);
+            throw new ArgumentException(e.Message);
+        }
     }
 }
